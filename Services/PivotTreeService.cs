5e95c4d5149d10128fa1d4012edfb7175ef990ec
Connie Mnemonic <conniemnemonic@gmail.com>
2021-08-22 22:54:48 +0100

    Some sanity

--- Services/PivotTreeService.cs
+++ Services/PivotTreeService.cs
@@ -30,30 +30,42 @@ namespace Ballerina.Services
         {
             var tree = new PivotTree(valueColumn);
 
-            var distinctPKs = GetDistinct(input, primaryKeyColumns);
+            var combinedPivotColumns = primaryKeyColumns.ToList();
+            combinedPivotColumns.AddRange(pivotColumns);
+            var combinedWithData = new List<string>(combinedPivotColumns);
+            combinedWithData.Add(valueColumn);
+            var distinctPivotSet = GetDistinct(input, combinedPivotColumns);
 
             //For each unique PK combo, generate tree
             //Assume moving left to right in tabular space -> moving deeper in tree space
 
-            foreach(DataRow row in distinctPKs.Rows)
+            foreach(DataRow row in distinctPivotSet.Rows)
             {
                 PivotTreeNode activeNode = tree.Root;
 
                 foreach(DataColumn column in row.Table.Columns)
                 {
+                    bool isValueColumn = column.ColumnName == valueColumn;
+
                     CellValue cellValue = new CellValue(row[column], column.DataType);
                     var next = activeNode.GetNext(cellValue);
+
                     if(next == null)
                     {
                         next = new PivotTreeNode(column.ColumnName, cellValue);
+
+                        if(isValueColumn)
+                        {
+                            //Leaf value is now redundant?
+                            next.LeafValue = cellValue;
+                        }
+
                         activeNode.AddChild(next);
                     }
 
                     activeNode = next;
                 }
             }
-
-            //TODO: Pivot columns
 
             return tree;
         }
