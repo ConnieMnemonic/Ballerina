1b319a83c8415717d6e14da3a12c4fd89ec59ddd
Connie Mnemonic <conniemnemonic@gmail.com>
2021-08-22 21:35:54 +0100

    Basic garbage

---
+++ Ballerina.cs
@@ -0,0 +1,13 @@
+using System.Data;
+using System.Collections.Generic;
+
+namespace Ballerina
+{
+    public class Ballerina
+    {
+        public DataTable Pivot(DataTable input, IEnumerable<string> pivotColumns, IEnumerable<string> valueColumns, string primaryKey)
+        {
+            
+        }
+    }
+}
